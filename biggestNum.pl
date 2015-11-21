eabd4a6aa4ddf19770d2bb9d6df67e47cdc3affc
Abhimanyu Bishnoi <manyubishnoi@gmail.com>
2015-11-21 13:11:41 -0800

    finds the largest number from the combination of many small numbers

---
+++ biggestNum.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+#This program combines all the individual integer elements of the array and then produces the largest
+#possible number from the combination.
+
+use strict;
+use warnings;
+
+my @arr = (3,6,12,45,87,234,67,9,1);
+my $concat;
+my $sorted;
+
+foreach my $x (@arr) {
+	print "element: $x\n";
+	$concat .= $x;
+}
+
+print "Concat is $concat\n";
+
+my @arr1 = split(//, $concat);
+$sorted = reverse sort @arr;
+
+print"arr1 is @arr1\n";
+print "sorted concat is: $sorted\n";
+
