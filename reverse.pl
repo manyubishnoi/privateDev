a6d17103b1ab82742409c3f3e725d68a40b1faee
Abhimanyu Bishnoi <manyubishnoi@gmail.com>
2015-11-29 23:19:59 -0800

    ways to reverse a string

--- reverse.pl
+++ reverse.pl
@@ -5,14 +5,27 @@
 use strict;
 use warnings;
 
-my $s = "Hhello World";
+my $string = "Hhello World";
 my $i;
 my $j;
+my $s;
+my $rev;
+my @arr = split(//, $string);
 
-my @arr = split(//, $s);
+# --------------- Method 1 -----------------------#
+$string =~ s/(.)/substr($string, length($string)-pos($string)-1, 1)/eg;
+print "Method 1 output: $string\n";
+
+# --------------- Method 2 -----------------------#
+for ($s = $#arr; $s >= 0; $s--) {
+        $rev .= $arr[$s];
+}
+print "Method 2 output: $rev\n";
+
+# --------------- Method 3 -----------------------#
 
 for ($i = 0, $j = $#arr; $i < $#arr/2, $j > $#arr/2; $i++, $j--) {
 	@arr[$i,$j] = @arr[$j,$i];
 }
-
-print "reversed array is @arr\n";
+my $join = join ("", @arr);
+print "Method 3 output:  $join\n";
